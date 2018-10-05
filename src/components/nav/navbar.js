6fc07fa224bf0908ce1c88a3f337682e8038df55
ac5tin <austinchang4@gmail.com>
2018-10-05 18:06:07 +0100

    changed logo and added navburger

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -3,16 +3,31 @@ import React from 'react';
 const Navbar = () => (
     <div id="navigationBar">
         <nav className="navbar is-white" role="navigation" aria-label="main navigation">
-            <div className="navbar-menu">
-                <div className="navbar-start">
-                    <h2 className="navbar-item">ac5</h2>
+            <div className="navbar-brand">
+                {/* <h2 className="navbar-item">ac5</h2> */}
+                <img id="logo" className="navbar-item" src="https://cdn.discordapp.com/attachments/392845290341924875/497814875511193610/tins_logo.png"  />
+                
+
+
+                <div className="navbar-burger burger" data-target="navBurger"  onClick={e=>{
+                e.target.classList.toggle('is-active');
+                document.querySelector('#navBurger').classList.toggle('is-active');
+            }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
+            </div>
+            <div className="navbar-menu" id="navBurger">
+                <div className="navbar-start"></div>
                 <div className="navbar-end">
                     <a className="navbar-item">about</a>
                     <a className="navbar-item">work</a>
                     <a className="navbar-item">skills</a>
                 </div>
+                
             </div>
+            
             
         </nav>
     </div>
